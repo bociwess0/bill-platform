7a2aa1300a8b6e05a322df90a7419f76ff516bc2
bociwess0 <bogdandjordjevic2000@gmail.com>
2025-08-30 12:22:26 +0200

    handling the errors and adding the spinner while data is fetched

--- src/api/DatabaseRequests/Requests.ts
+++ src/api/DatabaseRequests/Requests.ts
@@ -1,12 +1,19 @@
 import axios, { AxiosResponse } from "axios";
 import { Bill, Result } from "../../Interfaces/Interface";
 
-//base API url 
+//base API url
 axios.defaults.baseURL = "https://api.oireachtas.ie/v1/";
 
 // function that fetches all the bills from the api
 export async function fetchBills(): Promise<Bill[]> {
+  try {
     const response: AxiosResponse<any> = await axios.get("legislation");
     const bills: Bill[] = response.data.results.map((r: Result) => r.bill);
+
     return bills;
+
+  } catch (error) {
+    console.log("Error fetching product data!");
+    throw error;
+  }
 }
--- src/components/BillTable.tsx
+++ src/components/BillTable.tsx
@@ -1,9 +1,18 @@
+/**
+ * BillTable.tsx
+ *
+ * This component displays a table of bills retrieved from the database.
+ * It fetches the bill data when the component mounts, stores it in state,
+ * and renders it in a Material-UI table format.
+ */
+
 import { useEffect, useState } from "react";
 import { fetchBills } from "../api/DatabaseRequests/Requests";
 import { Bill } from "../Interfaces/Interface";
 import {
-    Container,
-    IconButton,
+    CircularProgress,
+  Container,
+  IconButton,
   Paper,
   Table,
   TableBody,
@@ -15,18 +24,39 @@ import {
 import { FavoriteBorder } from "@mui/icons-material";
 
 export default function BillTable() {
-  const [bills, setBills] = useState<Bill[]>([]);
+  
+  const [bills, setBills] = useState<Bill[]>([]); // State variable for storing all bills fetched from the database
+  const [error, setError] = useState<string | null>(null); // State variable for storing the error message
+  const [loading, setLoading] = useState<boolean>(true); // State variable for controling the loading action
 
+  //getting all the bills from database when the component mounts
   useEffect(() => {
     async function getBills() {
-      const fetchedBills: Bill[] = await fetchBills();
-      setBills(fetchedBills);
-      console.log(fetchedBills);
+      try {
+        const fetchedBills: Bill[] = await fetchBills(); //calling the fetch method
+        setBills(fetchedBills); // storting the bills into state
+      } catch (err) {
+        setError("Failed to fetch bills. Please try again later.") //storing the error message into state 
+        console.log(err);
+      } finally {
+        setLoading(false) //stopping the loader
+      }
     }
 
     getBills();
   }, []);
 
+  //showing the spinner icon until the bills are fetched
+  if(loading) {
+    return <Container><CircularProgress/></Container>
+  }
+
+  //if there was an error which occured while fetching the data, the error message will be shown
+  if(error) {
+    return <Container>{error}</Container>
+  }
+
+  //rendering the table of bills (bill number, type, status, sponsor and favorite action button)
   return (
     <Container>
       <TableContainer component={Paper}>
@@ -52,9 +82,13 @@ export default function BillTable() {
                 <TableCell>{bill.billNo}</TableCell>
                 <TableCell>{bill.billType}</TableCell>
                 <TableCell>{bill.status}</TableCell>
-                <TableCell>{bill.sponsors?.[0].sponsor?.by?.showAs || "/"}</TableCell>
                 <TableCell>
-                    <IconButton><FavoriteBorder /></IconButton>
+                  {bill.sponsors?.[0].sponsor?.as?.showAs || "/"}
+                </TableCell>
+                <TableCell>
+                  <IconButton>
+                    <FavoriteBorder />
+                  </IconButton>
                 </TableCell>
               </TableRow>
             ))}
